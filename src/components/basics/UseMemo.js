c15560c0c29d72004daca37ff4f15c8eba95c650
Devashish Mali <devashish.mali06@gmail.com>
2024-04-16 14:34:46 +0530

    useMemo added

--- src/components/basics/UseMemo.js
+++ src/components/basics/UseMemo.js
@@ -1,32 +1,42 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 const UseMemo = () => {
   const [counterOne, setCounterOne] = useState(0);
   const [counterTwo, setCounterTwo] = useState(0);
 
   const incrementOne = () => {
-    let i = 0;
-    while (i < 1000000) {
-      i++;
-    }
     setCounterOne(counterOne + 1);
   };
+
   const incrementTwo = () => {
     setCounterTwo(counterTwo + 1);
   };
-  const isEven = (num) => {
-    return num % 2 === 0;
-  };
+
+  // const isEven = (num) => {
+  //   console.log("even called")
+  //   let i = 0;
+  //   while (i < 1000000000) {
+  //     i++;
+  //   }
+  //   return num % 2 === 0;
+  // };
+
+  const isEven = useMemo(() => {
+    console.log("even called");
+    let i = 0;
+    while (i < 1000000000) {
+      i++;
+    }
+    return counterOne % 2 === 0;
+  }, [counterOne]);
 
   return (
     <div>
       <h2>UseMemo Hook</h2>
       <h3 style={{ textAlign: "center" }}>
-        Counter One - {counterOne} is {isEven(counterOne) ? "Even" : "Odd"}
+        Counter One - {counterOne} is {isEven ? "Even" : "Odd"}
       </h3>
-      <h3 style={{ textAlign: "center" }}>
-        Counter Two - {counterTwo} is {isEven(counterTwo) ? "Even" : "Odd"}
-      </h3>
+      <h3 style={{ textAlign: "center" }}>Counter Two - {counterTwo}</h3>
       <button type="button" onClick={incrementOne}>
         INC One
       </button>
